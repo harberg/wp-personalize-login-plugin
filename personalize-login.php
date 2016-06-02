f1746d0cfe9268f6830b2b991321c5f2cdb72197
Nick Harberg <nick@seamonsterstudios.com>
2016-06-02 14:07:43 -0700

    initial commit

---
+++ personalize-login.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name : Personalize Login
+ * Description : A plugin that replaces the WordPress login flow with a custom page
+ * Version     : 1.0.0
+ * Author      : Nick Harberg
+ * License     : MIT
+ * Text Domain : personalize-login
+ */
